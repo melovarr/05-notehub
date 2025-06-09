2a5f74bf4a2409e4cd999de9af2a012863921e55
Serhii Zhelobkov <meeelnik@gmail.com>
2025-06-09 22:22:09 +0300

    more fixing

--- src/components/NoteModal/NoteModal.tsx
+++ src/components/NoteModal/NoteModal.tsx
@@ -5,6 +5,7 @@ import NoteForm from '../NoteForm/NoteForm';
 
 interface NoteModalProps {
   onClose: () => void;
+  children: React.ReactNode;
 }
 
 export default function NoteModal({ onClose }: NoteModalProps) {
