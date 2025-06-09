d59637386a8dba6bf6580b2bc4bcaa4d2be5410b
Serhii Zhelobkov <meeelnik@gmail.com>
2025-06-09 13:24:42 +0300

    deleted index.css from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import App from './components/App/App';
 import 'modern-normalize/modern-normalize.css';
-// import './global.css';
+import './global.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
