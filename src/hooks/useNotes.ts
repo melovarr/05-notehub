736dd97168a32adde149487e59e6f3469c3aa01c
Serhii Zhelobkov <meeelnik@gmail.com>
2025-06-09 12:49:56 +0300

    deleted hooks folder

--- src/hooks/useNotes.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { getNotes } from '../services/noteService';
-
-export const useNotes = (query: string) => {
-  return useQuery({
-    queryKey: ['notes', query],
-    queryFn: () => getNotes(query),
-  });
-};
